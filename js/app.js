15b5a4eb6cd73208ae986ff00e118c8028276a65
Rakthy Thip <campinamerica@yahoo.com>
2019-09-19 16:49:19 -0700

    use foreach instead of loops

--- js/app.js
+++ js/app.js
@@ -2,24 +2,47 @@ const searchContainer = document.querySelector(".search-container");
 const gallery = document.getElementById("gallery");
 
 //fetch function//
-fetch("https://randomuser.me/api/?results=5000")
+fetch("https://randomuser.me/api/?results=12?")
   .then(response => response.json())
   .then(data => generateDom(data.results));
 
 //helper functions//
-const generateDom = result => {
-  for (let i = 0; i < result.length; i++) {
-    const html = `
-    <div class="card">
-              <div class="card-img-container">
-                  <img class="card-img" src= ${result[i].picture.large} alt="profile picture"'>
-              </div>
-              <div class="card-info-container">
-                  <h3 id="name" class="card-name cap">${result[i].name.first} ${result[i].name.last}</h3>
-                  <p class="card-text">${result[i].email}</p>
-                  <p class="card-text cap">${result[i].location.city}</p>
-              </div>
-          </div>`;
-          gallery.innerHTML = html;
-  }
+const generateDom = results => {
+  let html = '';
+results.forEach(result =>  {
+html += `<div class='card'>
+                    <div class="card-img-container">
+                        <img class="card-img" src= ${result.picture.large} alt="profile picture"'>
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${result.name.first} ${result.name.last}</h3>
+                        <p class="card-text">${result.email}</p>
+                        <p class="card-text cap">${result.location.city}, ${result.location.state}</p>
+                    </div>
+                </div> `;
+              });
+    gallery.innerHTML = html;
 };
+
+
+
+
+// const search = results => {
+//   results.forEach(result => {
+//     const inputValue = result.name.first.toLowerCase();
+//     const employee = result.name.first.toLowerCase();
+//     if (inputValue.includes(employee)) {
+//       .style.display = "flex";
+//     } else {
+//       .style.display = "none";
+//     }
+//   });
+// };
+
+// const appendSearchInput = () => {
+//   const html = `<form action="#" method="get">
+//               <input type="search" id="search-input" class="search-input" placeholder="Search...">
+//               <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+//           </form>`;
+//   searchContainer.innerHTML = html;
+// };
