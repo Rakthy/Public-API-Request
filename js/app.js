8867b9e1079b8175b4d9cbb5a1870654319c2fdf
Rakthy Thip <campinamerica@yahoo.com>
2019-09-23 10:57:27 -0700

    modal pop-up

--- js/app.js
+++ js/app.js
@@ -4,22 +4,12 @@ const gallery = document.getElementById("gallery");
 fetch("https://randomuser.me/api/?results=12&nat=us")
   .then(response => response.json())
   .then(data => generateCards(data.results));
-
 //append search input//
-const appendSearchInput = () => {
-  const html = `<form action="#" method="get">
-                <input type="search" id="search-input" class="search-input" placeholder="Search...">
-                <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
-            </form>`;
-  searchContainer.innerHTML = html;
-};
-
-appendSearchInput();
 
 //card function to generate random employees to DOM//
 const generateCards = results => {
   let html = "";
-  results.forEach(result => {
+  results.forEach((result, index) => {
     html += `<div class='card'>
                     <div class="card-img-container">
                         <img class="card-img" src= ${
@@ -46,11 +36,12 @@ const generateCards = results => {
   });
 };
 
-//append modal//
-
+const modalContainer = document.createElement("div");
 const generateModal = results => {
+  modalContainer.className = "modal-container";
+  document.body.appendChild(modalContainer);
   let html = "";
-  results.forEach(result => {
+  results.forEach((result, index) => {
     html = `  <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -77,13 +68,36 @@ const generateModal = results => {
                 </div>`;
   });
 
-  document.body.innerHTML += html;
+  modalContainer.innerHTML = html;
   const modal = document.querySelector(".modal-container");
   const closeBtn = document.querySelector(".modal-close-btn");
+  modal.style.display = "";
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
 };
+
+// <div class="modal-container">
+//     <div class="modal">
+//         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//         <div class="modal-info-container">
+//             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+//             <h3 id="name" class="modal-name cap">name</h3>
+//             <p class="modal-text">email</p>
+//             <p class="modal-text cap">city</p>
+//             <hr>
+//             <p class="modal-text">(555) 555-5555</p>
+//             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+//             <p class="modal-text">Birthday: 10/21/2015</p>
+//         </div>
+//     </div>
+//
+//     // IMPORTANT: Below is only for exceeds tasks
+//     <div class="modal-btn-container">
+//         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//         <button type="button" id="modal-next" class="modal-next btn">Next</button>
+//     </div>
+// </div>
 
 // StackoverFlow Function capitalize first letter of every string//
 const capitalize = phrase => {
